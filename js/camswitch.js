cc77d62ab47b224d1b81342482f9298f9a9adace
circlefour <0100.circle@gmail.com>
2023-12-11 13:56:58 -0500

    dropdown wip

--- js/camswitch.js
+++ js/camswitch.js
@@ -1,14 +1,13 @@
 const camSelect = document.getElementById('camSelect');
 
 joinButton.addEventListener('click', () => {
-    meeting.localParticipant.on("stream-enabled", (stream) => {
-        camSelect.style.display = 'block';
-        listCams();
-    });  
+    listCams();
+    camSelect.style.display = 'block';
 });
 
 async function listCams() {
     try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
         const devices = await meeting?.getWebcams();
         console.log(devices);
         camSelect.innerHTML = '';
@@ -16,8 +15,10 @@ async function listCams() {
         devices.forEach((device) => {
             const option = document.createElement('option');
             option.value = device.deviceId;
-            option.text = device.label || `camera ${cameraDropdown.options.length + 1}`;
+            option.text = device.label || `camera ${camSelect.options.length + 1}`;
             camSelect.appendChild(option);
+        // stop the stream as it was only used to get camera access permission
+        stream.getTracks().forEach(track => track.stop());
         });
     } catch(error) {
         console.log(error);
